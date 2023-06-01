64cb38457e346d6804f783b5b1ef0a1374ae6c57
Karla Cruz <karliox1191@gmail.com>
2023-05-31 23:14:14 -0600

    Modificaciones a clase model

--- src/main/java/com/kc/spring/mvc/model/User.java
+++ src/main/java/com/kc/spring/mvc/model/User.java
@@ -1,14 +1,6 @@
 package com.kc.spring.mvc.model;
 
 public class User {
-	private String userName;
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	private String username;
 	
 }
